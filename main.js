9311a1a63dfff3f95c0c5ba3c18210098a3b3c1c
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-12-02 21:36:43 -0500

    [ADD] Object.defineProperty

--- main.js
+++ main.js
@@ -9,16 +9,52 @@ const orlando = {
     }
 };
 
-console.log(Object.keys(orlando));
-console.log(Object.getOwnPropertyNames(orlando));
-console.log(Object.entries(orlando)); 0
+// console.log(Object.keys(orlando));
+// console.log(Object.getOwnPropertyNames(orlando));
+// console.log(Object.entries(orlando)); 
 
-console.log(Object.getOwnPropertyDescriptors(orlando));
+// // console.log(Object.getOwnPropertyDescriptors(orlando));
 
-Object.defineProperty(orlando, "pruebaDefineProperty", {
-    value: "java script es lo mejor",
-    enumerable: true,
-    writable: true,
+// Definir propiedad no enumerable, no editable, no configurable
+Object.defineProperty(orlando, "pruebaDefinePropertyFalse", {
+    value: "esteEsFalse",
+    enumerable: false,
+    writable: false,
+    configurable: false
 });
 
+// Propiedad no enumerable, pero editable y configurable
+Object.defineProperty(orlando, "navigator", {
+    value: "Chrome",
+    enumerable: false,
+    writable: true,
+    configurable: true
+});
+
+// Propiedad enumerable, no editable, pero configurable
+Object.defineProperty(orlando, "editor", {
+    value: "Visual Studio Code",
+    enumerable: true,
+    writable: false,
+    configurable: true
+});
+
+// Propiedad enumerable, editable, no configurable
+Object.defineProperty(orlando, "terminal", {
+    value: "WSL",
+    enumerable: true,
+    writable: true,
+    configurable: false
+});
+
+// Ver descriptores de propiedades actuales
+console.log(Object.getOwnPropertyDescriptors(orlando));
+
+// Sellar el objeto: no permite agregar o eliminar propiedades
+Object.seal(orlando);
+
+// Congelar el objeto: no permite modificar nada
+Object.freeze(orlando);
+
+// Ver descriptores tras aplicar `seal` y `freeze`
 console.log(Object.getOwnPropertyDescriptors(orlando));
